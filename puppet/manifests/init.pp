07232da816bd566294867920a6e757aa7de28506
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2015-09-20 09:51:47 +0200

    refs #490 - Typo

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,12 +1,12 @@
 class echoes_alert_dbo (
-  $branch  = $echoes_alert_dbo::params::branch, 
+  $branch  = $echoes_alert_dbo::params::branch,
   $version = $echoes_alert_dbo::params::version
 ) inherits echoes_alert_dbo::params {
   validate_string($branch)
   validate_string($version)
 
   require wt
-  
+ 
   anchor { "${module_name}::begin": } ->
   class { "${module_name}::install": } ->
   anchor { "${module_name}::end": }
