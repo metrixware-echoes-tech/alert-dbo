a10c13e245c2f83b264e2058ebc0c36a014e7d47
Thomas <thomas@echoes.fr>
2017-02-24 18:25:40 +0100

    opensourcing : changing headers for cpp files

--- src/tools/SpecializedAuthService.cpp
+++ src/tools/SpecializedAuthService.cpp
@@ -1,16 +1,25 @@
-/* 
- * Gui SpecializedAuthService.cpp
- * 
- * @author Echoes Technologies
- * @author Thomas SAQUET (TSA)
- * 
- * @date 13/08/2014
- * 
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO ECHOES TECHNOLOGIES SAS
- * AND MAY NOT BE REPRODUCED, PUBLISHED OR DISCLOSED TO OTHERS WITHOUT
- * COMPANY AUTHORIZATION.
- * AbstractPage
- * COPYRIGHT 2012-2014 BY ECHOES TECHNOLGIES SAS
+/**
+ * @file SpecializedAuthService.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - Dbo is the database objects library, part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
  */
 
@@ -53,4 +62,3 @@ void SpecializedAuthService::sendMail(const Wt::Mail::Message& message) const
     client.send(m);
   
 }
-
